04b874a3a2317affc1f02ed0b9fc58dd566e98a8
jos3duardo <jos3duardolopes@gmail.com>
2025-08-09 22:36:31 -0400

    Refactor import paths for PaymentsService to improve module organization

--- src/modules/payments/tests/payments.controller.spec.ts
+++ src/modules/payments/tests/payments.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PaymentsController } from '../payments.controller';
-import { PaymentsService } from '../payments.service';
+import { PaymentsService } from '../services/payments.service';
 
 describe('PaymentsController', () => {
   let controller: PaymentsController;
--- src/modules/payments/tests/payments.service.spec.ts
+++ src/modules/payments/tests/payments.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { PaymentsService } from '../payments.service';
+import { PaymentsService } from '../services/payments.service';
 
 describe('PaymentsService', () => {
   let service: PaymentsService;
