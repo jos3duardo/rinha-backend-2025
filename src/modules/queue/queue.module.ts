1116181d8cc08ea5529089b85aca05569378186b
jos3duardo <jos3duardolopes@gmail.com>
2025-08-09 22:36:55 -0400

    Refactor QueueModule to use PAYMENT_QUEUE constant and update Redis connection settings

--- src/modules/queue/queue.module.ts
+++ src/modules/queue/queue.module.ts
@@ -1,19 +1,22 @@
 import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
+import { QueueService } from './queue.service';
+import { PAYMENT_QUEUE } from './constants/queue.constants';
 
 @Module({
   imports: [
     BullModule.forRoot({
       connection: {
-        host: '192.168.1.112',
+        host: 'redis',
         port: 6379,
-        username: 'default',
-        password: '7TfhqvHPe98AQq123',
       },
+    }),
+    BullModule.registerQueue({
+      name: PAYMENT_QUEUE,
     }),
   ],
   controllers: [],
-  providers: [],
-  exports: [],
+  providers: [QueueService],
+  exports: [QueueService],
 })
 export class QueueModule {}
