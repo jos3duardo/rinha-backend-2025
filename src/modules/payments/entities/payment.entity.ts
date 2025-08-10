b6ef03c54d723a772cd7f5e40f22685e966c56aa
jos3duardo <jos3duardolopes@gmail.com>
2025-08-09 22:36:13 -0400

    Add status and errorMessage fields to Payment entity

--- src/modules/payments/entities/payment.entity.ts
+++ src/modules/payments/entities/payment.entity.ts
@@ -4,6 +4,7 @@ import {
   Entity,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { PaymentStatusEnum } from '../enumns/payment-status.enum';
 
 @Entity('payments')
 export class Payment {
@@ -13,11 +14,21 @@ export class Payment {
   @Column({ type: 'decimal', precision: 10, scale: 2 })
   amount: number;
 
-  @Column({ type: 'varchar', name: 'payment_processor' })
+  @Column({
+    type: 'enum',
+    enum: PaymentStatusEnum,
+    default: PaymentStatusEnum.PENDING,
+  })
+  status: PaymentStatusEnum;
+
+  @Column({ type: 'varchar', name: 'payment_processor', nullable: true })
   paymentProcessor: string;
 
   @Column({ type: 'uuid', name: 'correlation_id', unique: true })
   correlationId: string;
+
+  @Column({ type: 'text', nullable: true })
+  errorMessage: string;
 
   @CreateDateColumn({ type: 'timestamp', name: 'created_at' })
   createdAt: Date;
