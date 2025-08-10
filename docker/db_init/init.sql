c9e5fa8c2c6e9418fc83f47658123c3f270342f0
jos3duardo <jos3duardolopes@gmail.com>
2025-08-09 22:37:17 -0400

    Add status and error_message fields to payment_processor table

--- docker/db_init/init.sql
+++ docker/db_init/init.sql
@@ -2,7 +2,9 @@ CREATE UNLOGGED TABLE payments (
     id UUID PRIMARY KEY,
     correlation_id UUID NOT NULL,
     amount DECIMAL NOT NULL,
-    payment_processor VARCHAR(50) NOT NULL,
+    payment_processor VARCHAR(50,
+    status VARCHAR(50) NOT NULL,
+    error_message TEXT,
     created_at TIMESTAMP default now() NOT NULL
 );
 
