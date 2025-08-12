3c2c43106aef02c19b1f8b47fd45bbf68af183dc
jos3duardo <jos3duardolopes@gmail.com>
2025-08-12 09:09:25 -0400

    Refactor payment entity to use correlationId as primary key and remove unused fields

--- src/modules/payments/entities/payment.entity.ts
+++ src/modules/payments/entities/payment.entity.ts
@@ -1,37 +1,15 @@
-import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
-import { PaymentStatusEnum } from '../enumns/payment-status.enum';
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
 
 @Entity('payments')
 export class Payment {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
+  @PrimaryColumn({ type: 'uuid', name: 'correlation_id', unique: true })
+  correlationId: string;
 
   @Column({ type: 'decimal', precision: 10, scale: 2 })
   amount: number;
 
-  @Column({
-    type: 'enum',
-    enum: PaymentStatusEnum,
-    default: PaymentStatusEnum.PENDING,
-  })
-  status: PaymentStatusEnum;
-
   @Column({ type: 'varchar', name: 'payment_processor', nullable: true })
   paymentProcessor: string;
-
-  @Column({ type: 'uuid', name: 'correlation_id', unique: true })
-  correlationId: string;
-
-  @Column({ type: 'text', nullable: true })
-  errorMessage: string;
-
-  @Column({ nullable: true })
-  attempts: number;
 
   @CreateDateColumn({ type: 'timestamp', name: 'created_at' })
   createdAt: Date;
